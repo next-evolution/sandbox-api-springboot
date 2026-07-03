ac02d12b6263f12a0e58e6d90503d411c60c041b
ne-tanjiro-kamado <tanjiro.kamado@next-evolution.co.jp>
2026-07-03 20:08:19 +0900

    ADD .claudeignore (#35)
    - その他軽微な修正

--- sandbox-api/src/main/java/jp/co/next_evolution/sandbox/api/dto/response/admin/UserResponse.java
+++ sandbox-api/src/main/java/jp/co/next_evolution/sandbox/api/dto/response/admin/UserResponse.java
@@ -1,7 +1,5 @@
 package jp.co.next_evolution.sandbox.api.dto.response.admin;
 
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jp.co.next_evolution.sandbox.api.dto.response.ApiResponse;
 import jp.co.next_evolution.sandbox.application.dto.user.UserDto;
